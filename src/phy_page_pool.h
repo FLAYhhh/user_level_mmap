b6c1d7bde2546a9fe3ffe5dc8495e6d5de26161e
kazuki <somekernel@gmail.com>
2024-03-08 09:42:18 +0800

    add phy_page_pool

--- src/phy_page_pool.h
+++ src/phy_page_pool.h
@@ -1,5 +1,6 @@
 #include <atomic>
 #include <cassert>
+#include <cstdio>
 #include <iostream>
 #include <memory>
 #include <thread>
@@ -13,45 +14,67 @@ struct Page {
 
 class MemoryPool {
    public:
-    MemoryPool(size_t num_pools, size_t pagesPerPool, size_t page_size)
+    // default pool size = 1GiB = 262144 * 4096GiB
+    // default page size = 4KiB
+    MemoryPool(size_t num_pools = 8, size_t pagesPerPool = 262144,
+               size_t page_size = 4096)
         : num_pools_(num_pools), page_size_(page_size) {
-        m_localPools_.resize(num_pools);
-        m_local_remain_pages_.resize(num_pools);
+        local_pools_.resize(num_pools);
+        local_remain_pages_.resize(num_pools);
+        pages_per_pool_.resize(num_pools, 0);
         for (size_t i = 0; i < num_pools; i++) {
-            m_localPools_[i] = new std::atomic<Page*>();
-            m_local_remain_pages_[i] = new std::atomic<size_t>();
+            local_pools_[i] = new std::atomic<Page*>();
+            local_remain_pages_[i] = new std::atomic<size_t>(0);
         }
         for (size_t pid = 0; pid < num_pools * pagesPerPool; pid++) {
-            Page* page = static_cast<Page*>(std::malloc(sizeof(Page)));
+            Page* page =
+                static_cast<Page*>(std::aligned_alloc(page_size, page_size));
+            page->next.store(nullptr);
+            assert((size_t)page % page_size == 0);
             if (page != nullptr) {
                 size_t pool_idx = ((size_t)page / page_size) % num_pools;
+                pages_per_pool_[pool_idx]++;
                 page->next.store(
-                    m_localPools_[pool_idx]->load(std::memory_order_relaxed),
+                    local_pools_[pool_idx]->load(std::memory_order_relaxed),
                     std::memory_order_relaxed);
-                m_localPools_[pool_idx]->store(page, std::memory_order_relaxed);
-                m_local_remain_pages_[pool_idx]++;
+                local_pools_[pool_idx]->store(page, std::memory_order_relaxed);
+                (*local_remain_pages_[pool_idx])++;
             } else {
                 throw std::bad_alloc();
             }
         }
 
         for (size_t i = 0; i < num_pools; ++i) {
-            assert(*m_local_remain_pages_[i] == pagesPerPool);
+            assert(*local_remain_pages_[i] == pages_per_pool_[i]);
         }
     }
 
     ~MemoryPool() {
-        for (std::atomic<Page*>* localPool : m_localPools_) {
-            Page* page = localPool->load(std::memory_order_relaxed);
+        size_t i = 0;
+        for (size_t i = 0; i < num_pools_; i++) {
+            assert(*local_remain_pages_[i] == pages_per_pool_[i]);
+        }
+
+        for (std::atomic<Page*>* localPool : local_pools_) {
+            Page* page = localPool->load(std::memory_order_seq_cst);
+            size_t free_cnt = 0;
             while (page != nullptr) {
-                Page* next = page->next.load(std::memory_order_relaxed);
+                // printf("pool[%zu]: free page, cnt(%zu): %p\n", i, free_cnt,
+                // page);
+                free_cnt++;
+                Page* next = page->next.load(std::memory_order_seq_cst);
                 std::free(page);
                 page = next;
             }
+
+            // printf("actual free: %zu, expected free: %zu", free_cnt,
+            // local_remain_pages_[i]->load());
+            assert(free_cnt == local_remain_pages_[i]->load());
+            i++;
         }
         for (size_t i = 0; i < num_pools_; i++) {
-            delete m_localPools_[i];
-            delete m_local_remain_pages_[i];
+            delete local_pools_[i];
+            delete local_remain_pages_[i];
         }
     }
 
@@ -60,23 +83,39 @@ class MemoryPool {
         size_t max_remain_pages = 0;
         // get a approximate richest pool
         for (size_t i = 0; i < num_pools_; i++) {
-            if (*m_local_remain_pages_[i] > max_remain_pages) {
-                max_remain_pages = *m_local_remain_pages_[i];
+            if (*local_remain_pages_[i] > max_remain_pages) {
+                max_remain_pages = *local_remain_pages_[i];
                 richest_pool_idx = i;
             }
         }
 
         Page* page =
-            m_localPools_[richest_pool_idx]->load(std::memory_order_relaxed);
+            local_pools_[richest_pool_idx]->load(std::memory_order_seq_cst);
+        Page* next = page->next.load(std::memory_order_seq_cst);
+        assert((size_t)page !=
+               0x2a2a2a2a2a2a2a2a);  // in test, we write 0x2a to page
+        /*
         while (page != nullptr &&
-               !m_localPools_[richest_pool_idx]->compare_exchange_weak(
+               !local_pools_[richest_pool_idx]->compare_exchange_weak(
                    page, page->next.load(std::memory_order_relaxed),
                    std::memory_order_acquire, std::memory_order_relaxed))
             ;
-        if (page == nullptr) {
-            throw std::bad_alloc();
+            */
+
+        while (page != nullptr && (size_t)page != 0x2a2a2a2a2a2a2a2a &&
+               !local_pools_[richest_pool_idx]->compare_exchange_strong(
+                   page, page->next.load(std::memory_order_seq_cst))) {
+            page =
+                local_pools_[richest_pool_idx]->load(std::memory_order_seq_cst);
+            next = page->next.load(std::memory_order_seq_cst);
         }
-        m_local_remain_pages_[richest_pool_idx]--;
+
+        (*local_remain_pages_[richest_pool_idx])--;
+
+        printf("page: %p, next: %p\n", page, next);
+        assert((size_t)page != 0x2a2a2a2a2a2a2a2a);
+        assert((size_t)next != 0x2a2a2a2a2a2a2a2a);
+
         return page;
     }
 
@@ -87,18 +126,28 @@ class MemoryPool {
             return;
         }
         Page* page = static_cast<Page*>(ptr);
-        page->next.store(m_localPools_[idx]->load(std::memory_order_relaxed),
-                         std::memory_order_relaxed);
-        Page* expected = page->next.load(std::memory_order_relaxed);
-        while (!m_localPools_[idx]->compare_exchange_weak(
-            expected, page, std::memory_order_release,
+        Page* expected = local_pools_[idx]->load(std::memory_order_seq_cst);
+        page->next.store(expected, std::memory_order_seq_cst);
+        /*
+        while (!local_pools_[idx]->compare_exchange_weak(
+            expected, page, std::memory_order_acquire,
             std::memory_order_relaxed))
             ;
+            */
+
+        while (!local_pools_[idx]->compare_exchange_strong(expected, page)) {
+            Page* expected = local_pools_[idx]->load(std::memory_order_seq_cst);
+            page->next.store(expected, std::memory_order_seq_cst);
+        }
+
+        printf("dealloc page %p\n", ptr);
+        (*local_remain_pages_[idx])++;
     }
 
    private:
     size_t num_pools_;
     size_t page_size_;
-    std::vector<std::atomic<Page*>*> m_localPools_;
-    std::vector<std::atomic<size_t>*> m_local_remain_pages_;
+    std::vector<size_t> pages_per_pool_;
+    std::vector<std::atomic<Page*>*> local_pools_;
+    std::vector<std::atomic<size_t>*> local_remain_pages_;
 };
